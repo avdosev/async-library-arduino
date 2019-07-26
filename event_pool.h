cfc60888ed9a74ef24639c6653c27e0bebc36d01
kyplyk <avdosev1@yandex.ru>
2019-07-26 20:22:24 +0400

    оптимизация #2

---
+++ event_pool.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <utility>
+#include <stdlib.h>
+
+#include "event.h"
+
+class EventPool {
+    private:
+        using event_pair = std::pair<event_id_t, event_t>;
+        size_t size, max_size;
+        event_pair* events;
+    public:
+        EventPool() {
+            size = 0; max_size = 10;
+            events = new event_pair[max_size];
+        }
+
+        ~EventPool() {
+            delete[] events;
+            events = nullptr;
+        }
+
+        event_id_t generateUniqueEventId() {
+            event_id_t id;
+            bool find;
+
+            do {
+                id = rand();
+                find = id == 0 && this->find(id) == this->end();
+            } while (find);
+
+            return id;
+        }
+
+        bool hasReadyEvent() {
+            for (auto map_item : *this) {
+                event_t item = map_item.second;
+                if (item->isReady()) return true;
+            }
+            return false;
+        }
+
+        std::pair<event_id_t, event_t> getReadyEvent() {
+            for (auto map_item : *this) {
+                event_t item = map_item.second;
+                if (item->isReady()) return map_item;
+            }
+            return event_pair(0, nullptr);
+        }
+
+        event_t getEvent(event_id_t id) {
+        }
+
+        event_id_t addEvent(event_t event) {
+            event_id_t id = generateUniqueEventId();
+            this->insert(std::make_pair(id, event));
+            event->startTracking();
+            return id;
+        }
+
+        void removeEvent(event_id_t id) {
+            for (size_t i = 0; i < size; i++) {
+                if (events[i].first == id) {
+                    for (size_t j = i; j < size-1; j++) {
+                        events[j] = events[j+1];
+                    }
+                    size--;
+                    break;
+                }
+            }
+        }
+
+        event_pair* begin() const{
+            return events;
+        }
+
+        event_pair* end() const {
+            return events+size;
+        }
+
+        event_pair* find(event_id_t id) {
+            for (auto it = this->begin(); it != this->end(); it++) {
+                event_t item = it->second;
+                if (item->isReady())
+                    return it;
+            }
+            return this->end();
+        }
+
+        event_pair* insert(event_pair pr) {
+            if (size == max_size) {
+                max_size = (max_size+1) * 2;
+                auto c_events = new event_pair[max_size];
+                for (size_t i = 0; i < size; i++) {
+                    c_events[i] = events[i];
+                }
+                delete[] events;
+                events = c_events;
+            }
+
+            events[size] = pr;
+            size++;
+        }
+};
